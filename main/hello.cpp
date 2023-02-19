da219143ad6bb66a46e226dfc8685b3a2870eb15
Nachiketh K S <81564939+nachikethks@users.noreply.github.com>
2023-02-19 18:34:45 +0530

    Updated

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World PES1UG20CS259\n";
+  cout << "Hello, World PES1UG20CS259-Nachiketh K S\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
